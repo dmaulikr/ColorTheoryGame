931fa29f34bc007d0f2d9a3983df079d298fcbeb
Dylan Schroder <dylan.schroder31@gmail.com>
2017-01-16 17:36:41 -0700

    Updated Master Branch
    Repaired scene files and prefabs
    Added color logic

---
+++ Assets/Scripts/ColorPreset.js
@@ -0,0 +1,29 @@
+﻿#pragma strict
+//presets the color of the ColorCircles. Used for designing individual levels
+//put this code onto the ColorCircles Object
+public var isBlue: int;
+public var isRed:int;
+public var isYellow:int;
+var colorer:SpriteRenderer;//drag the sprite/colorcircles into this.
+var objColor:Color;
+function Start () {
+	
+}
+
+function Update () {
+	if(isBlue==1){
+	objColor=new Color(0,0,1,1);
+	colorer.color=new Color(0,0,1,1);
+	gameObject.tag="Blue Dot";			//Use the tag system to check colors later on
+	}
+	else if(isRed==1){
+	objColor=new Color(0,0,1,1);
+	colorer.color=new Color(1,0,0,1);
+	gameObject.tag="Red Dot";
+	}
+	else if(isYellow==1){
+	objColor=new Color(0,0,1,1);
+	colorer.color=new Color(1,1,0,1);
+	gameObject.tag="Yellow Dot";
+	}
+}
---
+++ Assets/Scripts/Scale.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+public class Scale extends MonoBehaviour{
+
+	var s:float;
+
+	var num:float;	//numerator the closer this num is to 255 the larger it gets
+	var object:GameObject;	//This must take in the object it is assigned to
+	function Start () {
+		//scalar values must be from 0-1 using x/255 as scale
+		//default values set
+		//s=0f;	//This updates based on value of num
+		//num=1f;
+		s=(num/255f);
+		object.transform.localScale=new Vector3(s,s,s);
+
+	}
+
+	function Update () {
+
+	}
+	function getScale(){
+		return s;
+		
+	}
+}
